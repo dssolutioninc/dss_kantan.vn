6aa734add4180702672386714b800c947212fcf7
duluong <luongvandu@gmail.com>
2015-09-06 22:34:59 +0900

    Set4Prod

--- api/services/Constants.js
+++ api/services/Constants.js
@@ -13,11 +13,11 @@ module.exports = {
     bookNumOnHome: 3,
     bookNumOnHomeMore: 2,
     //Email
-    smtpServer: '52.68.197.24',
+    smtpServer: '52.76.88.187',
     smtpPort: 587,
     smtpUser: 'account',
     smtpPass: 'tugi2015',
-    sendFrom: 'account@congdongnhatngu.com',
+    sendFrom: 'account@kantan.vn',
 
     user: 'mrbotno@gmail.com',
     clientId: '109620886904-3i95hnuu084hoc937tnoikl818kd445k.apps.googleusercontent.com',
