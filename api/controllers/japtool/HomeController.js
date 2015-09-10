d47550216fa4bfacc56346714754ce5e1c2eb5aa
ducduongtmb <kieutrangths@gmail.com>
2015-09-10 13:04:40 +0700

    merge 2 page

--- api/controllers/japtool/HomeController.js
+++ api/controllers/japtool/HomeController.js
@@ -9,56 +9,57 @@ module.exports = {
     },
 
     index: function (req, res) {
-        if (!req.session.loadBookNum) {
-            req.session.loadBookNum = Constants.bookNumOnHome;
-        }
-
-        SelfLearning.count({where: {user: req.session.user.id}}).exec(function (err, count){
-            req.session.learningCount = count;
-
-            var noMore = false;
-            if (count <= req.session.loadBookNum) {
-                req.session.loadBookNum = count;
-                noMore = true;
-            }
-            SelfLearning.find({
-                where: {user: req.session.user.id},
-                limit: req.session.loadBookNum,
-                sort: {finishDate: 0, startDate: 0}
-            }).populate('bookMaster').exec(function (err, learnings) {
-                if (err) {
-                    if (err) return res.serverError(err);
-                } else {
-                    res.view('japtool/home/index', {listLearnings: learnings, noMore: noMore});
-                }
-            })
-        })
+        //if (!req.session.loadBookNum) {
+        //    req.session.loadBookNum = Constants.bookNumOnHome;
+        //}
+        //
+        //SelfLearning.count({where: {user: req.session.user.id}}).exec(function (err, count){
+        //    req.session.learningCount = count;
+        //
+        //    var noMore = false;
+        //    if (count <= req.session.loadBookNum) {
+        //        req.session.loadBookNum = count;
+        //        noMore = true;
+        //    }
+        //    SelfLearning.find({
+        //        where: {user: req.session.user.id},
+        //        limit: req.session.loadBookNum,
+        //        sort: {finishDate: 0, startDate: 0}
+        //    }).populate('bookMaster').exec(function (err, learnings) {
+        //        if (err) {
+        //            if (err) return res.serverError(err);
+        //        } else {
+        //            res.view('japtool/home/index', {listLearnings: learnings, noMore: noMore});
+        //        }
+        //    })
+        //})
+        res.redirect('/');
     },
 
-    loadMore: function (req, res) {
-        SelfLearning.find({
-            where: {user: req.session.user.id},
-            limit: Constants.bookNumOnHomeMore,
-            skip: req.session.loadBookNum,
-            sort: {finishDate: 0, startDate: 0}
-        }).populate('bookMaster').exec(function (err, learnings) {
-            if (err) {
-                if (err) return res.serverError(err);
-            } else {
-
-                if(learnings.length == '' || learnings.length == null){
-                    res.ok();
-                }else{
-                    req.session.loadBookNum += learnings.length;
-                    var noMore = (req.session.loadBookNum == req.session.learningCount);
-
-                    res.render('japtool/home/loadMore', {
-                        listLearnings: learnings, noMore: noMore
-                    });
-                }
-            }
-        })
-    },
+    //loadMore: function (req, res) {
+    //    SelfLearning.find({
+    //        where: {user: req.session.user.id},
+    //        limit: Constants.bookNumOnHomeMore,
+    //        skip: req.session.loadBookNum,
+    //        sort: {finishDate: 0, startDate: 0}
+    //    }).populate('bookMaster').exec(function (err, learnings) {
+    //        if (err) {
+    //            if (err) return res.serverError(err);
+    //        } else {
+    //
+    //            if(learnings.length == '' || learnings.length == null){
+    //                res.ok();
+    //            }else{
+    //                req.session.loadBookNum += learnings.length;
+    //                var noMore = (req.session.loadBookNum == req.session.learningCount);
+    //
+    //                res.render('japtool/home/loadMore', {
+    //                    listLearnings: learnings, noMore: noMore
+    //                });
+    //            }
+    //        }
+    //    })
+    //},
 
     learningDetail: function (req, res) {
         var learningID = req.param('learningID');
@@ -101,74 +102,104 @@ module.exports = {
         })
     },
 
-    loadSumary: function (req, res) {
-        // get lessons list by bookid
-        BookDetail.find({where: {bookMaster: req.param('bookID')}, sort: {sort: 1} }).
-        exec(function(err, lessons){
-
-            // get learn history of these lessons
-            UserLearnHistory.find({where: {user: req.session.user.id, selfLearning: req.param('learningID')}
-            }).exec(function(err, learnedLessons){
-
-                // console.log('learnedLessons: ' + JSON.stringify(learnedLessons));
-                var lessonList = [];
-
-                lessons.forEach(function(lesson){
-
-                    var learnedInfo = learnedLessons.filter(function(learnedLesson){
-                        return learnedLesson.bookDetail == lesson.id;
-                    });
-
-                    if (learnedInfo.length > 0){
-                        lesson.learnedInfo = learnedInfo[0];
-                    }
-
-                    // console.log('lesson.learnedInfo: ' + JSON.stringify(lesson.learnedInfo));
-                    lessonList.push(lesson);
-                });
-
-                var passedNum = 0;
-                var fasledNum = 0;
-                var lessonName = "";
-
-                for (var i = 0; i < lessonList.length; ++i){
-                    if ( lessonName == lessonList[i].lesson){
-                        // console.log('lessonName == lessonList[i].lesson' );
-
-                    } else {
-                        // console.log('lessonName != lessonList[i].lesson' );
-                        lessonName = lessonList[i].lesson;
-
-                        var subLessons = lessonList.filter(function(les){
-                            return les.lesson == lessonName;
-                        });
-                        // console.log('subLessons.length: ' +  subLessons.length);
-
-                        var allPass = true;
-                        var hasHistory = false;
-                        subLessons.forEach(function(subLesson){
-                            if (!subLesson.learnedInfo){
-                                allPass = false;
-                            } else {
-                                hasHistory = true;
-
-                                // console.log('subLesson.learnedInfo.mark: ' +  subLesson.learnedInfo.mark);
-                                if (!subLesson.learnedInfo.mark || subLesson.learnedInfo.mark < Constants.passMark){
-                                    allPass = false;
-                                }
-                            }
-                        });
-
-                        if (allPass){
-                            passedNum += 1;
-                        } else if (hasHistory) {
-                            fasledNum += 1;
-                        }
-                    }
-                }
-
-                res.send({passedNum: passedNum, fasledNum: fasledNum});
-            });
-        })
-    },    
+    //loadSumary: function (req, res) {
+    //    // get lessons list by bookid
+    //    BookDetail.find({where: {bookMaster: req.param('bookID')}, sort: {sort: 1} }).
+    //    exec(function(err, lessons){
+    //
+    //        // get learn history of these lessons
+    //        UserLearnHistory.find({where: {user: req.session.user.id, selfLearning: req.param('learningID')}
+    //        }).exec(function(err, learnedLessons){
+    //
+    //            // console.log('learnedLessons: ' + JSON.stringify(learnedLessons));
+    //            var lessonList = [];
+    //
+    //            lessons.forEach(function(lesson){
+    //
+    //                var learnedInfo = learnedLessons.filter(function(learnedLesson){
+    //                    return learnedLesson.bookDetail == lesson.id;
+    //                });
+    //
+    //                if (learnedInfo.length > 0){
+    //                    lesson.learnedInfo = learnedInfo[0];
+    //                }
+    //
+    //                // console.log('lesson.learnedInfo: ' + JSON.stringify(lesson.learnedInfo));
+    //                lessonList.push(lesson);
+    //            });
+    //
+    //            var passedNum = 0;
+    //            var fasledNum = 0;
+    //            var lessonName = "";
+    //
+    //            for (var i = 0; i < lessonList.length; ++i){
+    //                if ( lessonName == lessonList[i].lesson){
+    //                    // console.log('lessonName == lessonList[i].lesson' );
+    //
+    //                } else {
+    //                    // console.log('lessonName != lessonList[i].lesson' );
+    //                    lessonName = lessonList[i].lesson;
+    //
+    //                    var subLessons = lessonList.filter(function(les){
+    //                        return les.lesson == lessonName;
+    //                    });
+    //                    // console.log('subLessons.length: ' +  subLessons.length);
+    //
+    //                    var allPass = true;
+    //                    var hasHistory = false;
+    //                    subLessons.forEach(function(subLesson){
+    //                        if (!subLesson.learnedInfo){
+    //                            allPass = false;
+    //                        } else {
+    //                            hasHistory = true;
+    //
+    //                            // console.log('subLesson.learnedInfo.mark: ' +  subLesson.learnedInfo.mark);
+    //                            if (!subLesson.learnedInfo.mark || subLesson.learnedInfo.mark < Constants.passMark){
+    //                                allPass = false;
+    //                            }
+    //                        }
+    //                    });
+    //
+    //                    if (allPass){
+    //                        passedNum += 1;
+    //                    } else if (hasHistory) {
+    //                        fasledNum += 1;
+    //                    }
+    //                }
+    //            }
+    //
+    //
+    //            var bookMasterLessonNum = req.param('bookMasterLessonNum');
+    //            var startDate = new Date (req.param('startDate'));
+    //            var finishDate = new Date (req.param('finishDate'));
+    //            //recommend book each day
+    //            var oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
+    //            var TotalDays = Math.round(Math.abs((finishDate.getTime() - startDate.getTime()) / (oneDay)));
+    //            var eachDayLesson = Math.ceil(bookMasterLessonNum / TotalDays);
+    //            // status learning
+    //            // percent of current day on total day
+    //            var now = new Date();
+    //            var percentDay = Math.round(Math.abs((now.getTime() - startDate.getTime()) / (oneDay))) / TotalDays;
+    //            //percent of passed lesson on total lesson
+    //            var percentLesson = passedNum / bookMasterLessonNum;
+    //            var rs = percentLesson - percentDay;
+    //            // get status learning
+    //            var statusLearning ;
+    //            if (rs > 0 && rs < 0.2) {
+    //                statusLearning = req.__("you catch up time's learning");
+    //            } else if (rs > 0.2) {
+    //                statusLearning = req.__("you learn fast");
+    //            } else if (rs < 0) {
+    //                statusLearning = req.__("you learn slowly");
+    //            }
+    //            res.send({
+    //                passedNum: passedNum,
+    //                fasledNum: fasledNum,
+    //                eachDayLesson:eachDayLesson,
+    //                percentDay:percentDay*100,
+    //                statusLearning:statusLearning
+    //            });
+    //        });
+    //    })
+    //},
 }
