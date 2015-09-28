0a2a008b84075dfbe5dbab6295f7559eb54a032c
duluong <luongvandu@gmail.com>
2015-09-28 14:02:30 +0900

    improveKanjiPractise

--- api/controllers/japtool/KanjiController.js
+++ api/controllers/japtool/KanjiController.js
@@ -11,44 +11,47 @@ module.exports = {
 
     practice: function (req, res) {
         var extractDataCondition = req.param('condition');
-        // var CVExtractDataCondition = extractDataCondition.replace('lesson', 'Bài ');
-        // var arr = CVExtractDataCondition.split(',');
-        // var lessonn = arr[3];
-        // sails.log(lessonn);
+
         Kanji.selectByLevel({condition: extractDataCondition}, function (err, kanjis) {
             if (err) return res.send(err.status);
-            var min = 1;
-            var max = kanjis.length;
-            var option;
+
             kanjis.forEach(function (kanji, index) {
-                var randomArr = [];
-                for (var i = 0; randomArr.length < 3; i++) {
-                    var randomResult = Math.floor(Math.random() * (max - min) + min);
-                    if (!(randomArr.indexOf(kanjis[randomResult].hanviet) > -1) && randomResult != index) {
-                        option = {};
-                        option.hanviet = kanjis[randomResult].hanviet;
-                        option.description = kanjis[randomResult].description? kanjis[randomResult].description : '';
-                        randomArr.push(option);
+                var randomIndexes = [];
+
+                // add key index first
+                randomIndexes.push(index);
+
+                // add more random index
+                while (randomIndexes.length < 4) {
+                    var randomIndex = Math.floor( Math.random() * kanjis.length );
+                    if ( randomIndexes.indexOf(randomIndex) < 0 && randomIndex != index ) {
+                        randomIndexes.push(randomIndex);
                     }
                 }
 
-                // add key opption
-                option = {};
-                option.hanviet = kanji.hanviet;
-                option.description = kanji.description? kanji.description : '';
-                randomArr.push(option);
+                // change order randomly
+                randomIndexes.sort(function () {
+                    return Math.round(Math.random()) - 0.5;
+                });
 
-                // make opptions in random order
-                randomArr.sort();
+                // make options list from random index list
+                var options = [], option;
+                randomIndexes.forEach(function (i){
+                    option = {};
+                    option.hanviet = kanjis[i].hanviet;
+                    option.description = kanjis[i].description? kanjis[i].description : '';
+                    options.push(option);
+                })
+                // sails.log('options: ' + JSON.stringify(options) );
 
-                //console.log(kanji.hanviet, randomArr);
-                kanji.randomKanjis = randomArr;
+                kanji.randomKanjis = options;
             });
-            //Ramdom practice
+
+            // change order randomly
             kanjis.sort(function () {
                 return Math.round(Math.random()) - 0.5;
             });
-            // res.render('japtool/kanji/practice', {'kanjis': kanjis,'lessonn':lessonn});
+
             res.render('japtool/kanji/practice', {'kanjis': kanjis});
         });
     },
